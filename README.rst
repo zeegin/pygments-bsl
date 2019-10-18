389fe8398dc9ab41a852c1af473a682c7d19c6cf
Ingvar Vilkman <zeegin@zeegin.com>
2019-10-18 19:41:46 +0300

    init: initial

---
+++ README.rst
@@ -0,0 +1,28 @@
+pygments-json
+=============
+
+Pygments_ is a syntax highlighting tool that supports a wide range of
+languages and data formats.
+
+Syntax rools from https://github.com/1c-syntax/1c-syntax
+
+Install
+--------
+
+  $ pip install pygments-bsl
+
+Usage
+-------
+
+Pygment json highlighting is available without any further customization from code as well
+as from the pygementize command:
+
+   $ pygmentize your-file.bsl
+
+   ... beautifully formatted json will flow here
+
+
+.. _Pygments: http://pygments.org/
+.. _pygments-bsl: https://github.com/zeegin/pygments-bsl
+.. _PyPI: http://pypi.python.org/pypi
+.. _pip: http://www.pip-installer.org/
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pygments-bsl",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Ingvar Vilkman",
+    author_email = "zeegin@zeegin.com",
+    license='MIT',
+    description = "Pygments 1C (BSL) lexer",
+    classifiers=[
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['pygments'],
+    entry_points={
+        'pygments.lexers': [
+            'bsl=pygments-bsl:BSLLexer',
+        ]
+    },
+    
+)

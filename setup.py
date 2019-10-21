9e88a7c6771f29709a2705669fbb722a48b6063d
Ingvar Vilkman <zeegin@zeegin.com>
2019-10-21 16:53:11 +0300

    fix: add description

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pygments-bsl',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     author='Ingvar Vilkman',
     author_email='zeegin@zeegin.com',
     url='https://github.com/zeegin/pygments-bsl',
     license='MIT License',
     description='Pygments 1C (BSL) lexer',
+    long_description=long_description,
     classifiers=[
         'Environment :: Plugins',
         'Intended Audience :: Developers',
